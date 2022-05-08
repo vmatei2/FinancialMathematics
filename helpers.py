c69683a4e9f6dabe27a4a3ec7c41d704fa32ded5
Vlad Matei <vlad.mateei@gmail.com>
2022-05-08 22:36:52 +0100

    Implemented Option Pricing function for binomial tree

--- helpers.py
+++ helpers.py
@@ -27,6 +27,98 @@ def binomial_tree_price_two_steps_calculation(r, t, qu, payoff_up_up, qd, payoff
     return option_value
 
 
+def binomial_tree_calculation(r, S0, K, t, N, up_probability, down_probability, option_type="call"):
+
+    binomial_tree_dict = {}
+    binomial_tree_dict['0'] = [S0]
+
+    ### Step 1: Let's calculate the prices:
+    S1_u = S0 + (S0 * up_probability)
+    S1_d = S0 - (S0 * down_probability)
+    u = S1_u / S0
+    d = S1_d / S0
+    print("First step is to calculate the up and down values: ")
+    print("U = %f/%f = %f" % (S1_u, S0, u))
+    print("D = %f/%f = %f" % (S1_d, S0, d))
+
+    disc = np.exp(-r*t)
+    q =  (np.exp(r*t)-d)/(u-d)
+
+    print()
+    print("Now let's check the market is arbitrage free: d < e^rt < u")
+    print("%f < %f < %f" % (d, np.exp(r*t), u))
+    print()
+    print("Now let's calculate the risk neutral measure")
+    print("Qu = (e^rt-d)/(u-d)")
+    print("Qd = 1 - Qu")
+    print("%f - %f / (%f-%f) = %f" % (np.exp(r*t), d, u, d, q))
+    print()
+
+    print("Now computing the tree branches")
+
+    for i in range(N+1):
+        if i == 0:
+            binomial_tree_dict[i] = [S0]
+            print("At Step %f: price is: %f" % (i, binomial_tree_dict[i][0]))
+
+        elif i == 1:
+            previous_price = binomial_tree_dict[0][0]
+            binomial_tree_dict[i] = [previous_price - (previous_price * down_probability),
+                                        previous_price + previous_price * up_probability]
+            print("At Step %f: prices are: %f, %f" % (i, binomial_tree_dict[i][0], binomial_tree_dict[i][1]))
+
+        elif i == 2:
+            previous_price_down = binomial_tree_dict[i-1][0]
+            previous_price_up = binomial_tree_dict[i-1][1]
+            binomial_tree_dict[i] = [previous_price_down - (previous_price_down * down_probability), previous_price_down + (previous_price_down* up_probability),
+                                        previous_price_up + (previous_price_up * up_probability)]
+            print("At Step %f: prices are: "
+                  "%f, %f, %f" % (i, binomial_tree_dict[i][0], binomial_tree_dict[i][1],
+                                  binomial_tree_dict[i][2]))
+
+        elif i == 3:
+            previous_price_down_down = binomial_tree_dict[i-1][0]
+            previous_price_down_up = binomial_tree_dict[i-1][1]
+            previous_price_up_up = binomial_tree_dict[i-1][2]
+
+            binomial_tree_dict[i] = [previous_price_down_down - (previous_price_down_down * down_probability),
+                                     previous_price_down_down + (previous_price_down_down * up_probability),
+                                     previous_price_down_up + (previous_price_down_up * up_probability),
+                                     previous_price_up_up + (previous_price_up_up * up_probability)]
+
+            print("At Step %f: prices are: "
+                  "%f, %f, %f % f" % (i, binomial_tree_dict[i][0], binomial_tree_dict[i][1],
+                                  binomial_tree_dict[i][2], binomial_tree_dict[i][3]))
+
+    C = S0* d ** (np.arange(N, -1, -1)) * u ** (np.arange(0, N+1, 1))
+
+    # Initialise option values at maturity
+    price_at_maturity = np.array(binomial_tree_dict[N])
+    # payoffs at maturity
+    if option_type == "call":
+        payoffs = np.maximum(price_at_maturity - K, np.zeros(N+1))
+    elif option_type == "put":
+        payoffs = np.maximum(K - price_at_maturity, np.zeros(N+1))
+
+    C = S0 * d ** (np.arange(N, -1, -1)) * u ** (np.arange(0, N+1, 1))
+
+    C = np.maximum(K-C, np.zeros(N+1))
+
+    #  Step backwards through tree
+    price_list = []
+    for i in np.arange(N, 0, -1):
+
+        print(C[1:i+1])
+        print(C[0:i])
+        C = disc * (q * C[1:i+1] + (1-q) * C[0:i])
+        price_list.append(C)
+
+    print(C)
+
+
+
+
+
 def black_scholes(r, S, K, T, sigma, option_type="call"):
     """
     Calculate the Black Scholes price of an option
@@ -81,17 +173,25 @@ if __name__ == '__main__':
     binomial_tree_price_next_step(0.05, 1 / 3, 0.505, 0.988, 0, 0.988)
     binomial_tree_price_two_steps_calculation(0.05, 1 / 3, 0.505171, 13.483, 0.494829, 0, 0.988)
 
+    t = 1
+    N = 2
+    S0 = 50
+    K = 52
+    r = 0.05
+    p_up = 0.2
+    p_down = 0.2
 
+    binomial_tree_calculation(r, S0, K, t, N, p_up, p_down, "put")
+
+
+    ### Black Scholes Part
     S = 90
     K = 92
     r = 0.04
     sigma = 0.3
     T = 1/3
-
     c0 = black_scholes(r, S, K, T, sigma, "call")
-
     p0 = black_scholes(r, S, K, T, sigma, "put")
-
     print(verifiy_put_call_parity(c0, p0, S, K, T, r))
 
 
